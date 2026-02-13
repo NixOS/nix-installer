a0252cfd11eabedbe73161a321219d653b400173
Mic92 <96200+Mic92@users.noreply.github.com>
2026-02-13 08:57:05 +0000

    Bump Nix version

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
     crane.url = "github:ipetkov/crane/v0.20.0";
 
-    nix.url = "github:NixOS/nix/2.33.2";
+    nix.url = "github:NixOS/nix/2.33.3";
 
     flake-compat.url = "github:edolstra/flake-compat/v1.0.0";
 
